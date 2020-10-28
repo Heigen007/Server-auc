92c2b43f10c9f650318663f8001c4051b1310421
Heigen007 <grisakovr@gmail.com>
2020-10-28 10:36:07 +0600

    init

---
+++ front/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <Header />
+    <MainStr />
+  </div>
+</template>
+
+<script>
+import MainStr from '@/components/MainStr.vue'
+import Header from '@/components/Header.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    MainStr,
+    Header
+  },
+  data: function () {
+    return {
+    }
+  },
+  computed: {},
+  watch: {},
+  methods: {
+  },
+  async beforeMount () {
+  }
+}
+</script>
+
+<style>
+body{
+  background-color: #c0e3ff;
+  margin:0;
+  padding:0;
+}
+</style>
---
+++ front/src/components/Header.vue
@@ -0,0 +1,55 @@
+<template>
+<div class = "header">
+  <div>
+    <img class = "logo" src = "../assets/logo.png">
+  </div>
+  <div class = "buttons">
+    <div class = "but">Auth</div>
+    <div class = "but">Login</div>
+    <div class = "but">Auccion</div>
+  </div>
+</div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'MainStr',
+  props: {
+  },
+  data: function () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+.header{
+  height:50px;
+  background-color: aqua ;
+}
+.logo{
+  margin-left: 5vw;
+  height: 50px;
+  float: left;
+}
+.buttons{
+  margin-left: 25vw;
+  display: flex;
+  flex-direction: row;
+}
+.but{
+  font-size: 210%;
+  margin-left: 11vw;
+  padding:6px;
+  color: #5f6bff;
+}
+.but:hover{
+  background-color: #50d2d2;
+  border-radius: 20%;
+}
+</style>
