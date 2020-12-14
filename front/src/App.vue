76d6ab8139f3c17f114c54470ff0c0efbee265e6
Heigen007 <67007974+Heigen007@users.noreply.github.com>
2020-12-14 21:41:32 +0600

    init 2

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,73 +1,22 @@
 <template>
-  <div class="home">
-    <Header :user="user" :balance="balance"/>
-    <div class = "a"></div>
-    <router-view :balance="balance" :aucId="aucId" :auc="auc" :user="user" :root="root" :users="UsersList" @clickBTN="auth" @auccion="myauc" @money="money"></router-view>
+<div class="container" id = "12">
+  <box />
+  <strick />
   </div>
 </template>
 
 <script>
-import Header from '@/components/Header.vue'
-
+import strick from "@/components/Strick.vue"
+import box from "@/components/Box.vue"
 export default {
-  name: 'Home',
+  name:"A",
   components: {
-    Header
-  },
-  data: function () {
-    return {
-      root: 'http://localhost:3000/',
-      user: '',
-      MessagesList: null,
-      UsersList: [],
-      auc: [],
-      aucId: null,
-      balance: 0
-    }
-  },
-  computed: {},
-  watch: {},
-  methods: {
-    auth (a, b) {
-      this.user = a
-      this.balance = b
-    },
-    myauc (id) {
-      this.aucId = id
-    },
-    money (a) {
-      this.balance = a
-      console.log(this.balance)
-    }
-  },
-  async beforeMount () {
-    setInterval(async () => {
-      const response = await fetch(this.root)
-      if (response.ok) {
-        const data = await response.json()
-        this.UsersList = data.user
-        this.auc = data.auc
-      }
-    }, 1000)
+    strick,
+    box
   }
 }
 </script>
 
 <style>
-body{
-  background-color: #c0e3ff;
-  margin:0;
-  padding:0;
-  z-index: 10;
-}
-.a {
-  background: url(assets/img.jpg);
-  width: 100%;
-  background-size: cover;
-  height: 100vh;
-  position: fixed;
-  filter: blur(5px);
-  z-index: -10;
-  top:0;
-}
+
 </style>
---
+++ front/src/components/Strick.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="main">
+    asasdasasfafasf
+  </div>
+</template>
+
+<script>
+export default {
+  name:"B",
+  data: function () {
+    return {
+      strick: 0
+    }
+  },
+  methods: {
+  },
+  beforeMount() {
+  }
+}
+</script>
+
+<style>
+.main {
+    position: absolute;
+    font-size: 500%;
+    left: 0;
+    top: 0;
+}
+</style>
