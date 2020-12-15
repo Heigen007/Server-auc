a256a39bd99412deada38397008207983a7033d3
Heigen007 <67007974+Heigen007@users.noreply.github.com>
2020-12-15 19:51:12 +0600

    q

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,22 +1,43 @@
 <template>
 <div class="container" id = "12">
-  <box />
-  <strick />
+  <!-- <strick :str = "strV"/> -->
+  <!-- <box @str = "str"/> -->
+  <menger-sponge :size="300" :iteration="2" color="hsl(100, 0%, 50%)"/>
   </div>
 </template>
 
 <script>
-import strick from "@/components/Strick.vue"
-import box from "@/components/Box.vue"
+// import strick from "@/components/Strick.vue"
+// import box from "@/components/Box.vue"
+import { MengerSponge } from 'vue-fractals'
 export default {
   name:"A",
   components: {
-    strick,
-    box
+    // strick,
+    // box,
+    MengerSponge
+  },
+  data: function () {
+    return {
+      strV: 0
+    }
+  },
+  methods: {
+    str(str2){
+      if (str2 == 1){
+        this.strV += 1
+      }else{
+        this.strV = 0      
+      }
+    }
   }
 }
 </script>
 
 <style>
-
+.container{
+  display: flex;
+  flex-direction: column;
+  left:0;
+}
 </style>
--- front/src/components/Box.vue
+++ front/src/components/Box.vue
@@ -46,7 +46,7 @@ export default {
       box.style.transform = 
     'translateY(' + 0 + 'px)';
       },800);
-
+      this.$emit('str', 1)
     } else {
 
       document.querySelectorAll('.side')
@@ -60,7 +60,7 @@ export default {
       setTimeout(() => {
       box.style.transform = 'translateX(' + 0 + 'px)';
       },800);
-
+      this.$emit('str', 0)
     }
     }
   },
@@ -102,8 +102,7 @@ button{
   font-size: 10px;
 }
 .container {
- position: relative;
- margin: 200px auto;
+ margin: 100px auto;
  width: 300px;
  height: 300px;
  perspective: 700px;
