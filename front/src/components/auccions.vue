e335015b61a687f704cff9bf0d52cb6a9fa4f13a
Heigen007 <grisakovr@gmail.com>
2021-01-20 22:24:51 +0600

    Revert "init 2"
    This reverts commit 76d6ab8139f3c17f114c54470ff0c0efbee265e6.

---
+++ front/src/components/auccions.vue
@@ -0,0 +1,80 @@
+<template>
+<div>
+  <div class="app">Auccions</div>
+  <div class="buttons" v-for="(auc, key) in auc" :key="key">
+    <div class = "title but"><b>Название: </b>{{auc.title}}</div>
+    <div class="text but"><b>Описание: </b>{{auc.text}}</div>
+    <div class = "photos" v-for="(auc, key) in auc.photo" :key="key">
+      <img v-if="`${auc}` != 'null'" :src="`http://localhost:3000/${auc}`">
+    </div>
+    <div class="price but"><b>Цена: </b>{{auc.price}}</div>
+    <div class="step but"><b>Шаг: </b>{{auc.step}}</div>
+    <div v-if="`${auc.time.second}` < 10" class="time but"><b>Время окончания: </b>{{auc.time.minut}}:0{{auc.time.second}}</div>
+    <div v-else class="time but"><b>Время окончания: </b>{{auc.time.minut}}:{{auc.time.second}}</div>
+    <router-link to="/auccion" class="n"> <div @click="click(key)" :id="`${key}`" class="button">Войти</div> </router-link>
+  </div>
+</div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'MainStr',
+  props: {
+    auc: Array,
+    hour: Number,
+    minut: Number,
+    aucId: Number
+  },
+  methods: {
+    click (id) {
+      this.$emit('auccion', id)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.app{
+  text-align: center;
+  font-size: 400%;
+  color: #e28a61;
+}
+.but{
+  color:#8a2101;
+}
+.photos {
+}
+.button{
+    font-size: 4vh;
+    background-color: #ffa377;
+    outline: none;
+    border: none;
+    border-radius: 10px;
+    text-decoration: none;
+    color: #8a2101;
+    position: relative;
+    left:50%;
+    transform: translate(-50%, 0);
+    width: 20%;
+    text-align: center;
+}
+.button:hover{
+      background-color: #ffc6ab;
+}
+.n{
+  text-decoration: none;
+}
+.buttons{
+  text-align: center;
+  font-size: 250%;
+  border: 5px solid #8a2101;
+  margin-top: 2vh;
+  border-radius: 10px;
+  padding: 1vh;
+}
+img{
+  width: 15vw;
+}
+</style>
---
+++ front/src/components/login.vue
@@ -0,0 +1,107 @@
+<template>
+<div class = "reg" v-on:keyup.enter="submit">
+  <div class = "text">Вход</div>
+  <input type="text" id = 'log' autocomplete="off" placeholder="Логин:">
+  <input type="text" id = 'pas' autocomplete="off" placeholder="Пароль:">
+  <div class = "c"><button class = "but"  @click="click">Войти</button></div>
+  <div class="mistake" v-if="showmi">Введите верный логин и пароль!</div>
+</div>
+
+</template>
+
+<script>
+var pr = null
+export default {
+  name: 'HelloWorld',
+  props: {
+    users: Array,
+    root: String
+  },
+  data: function () {
+    return {
+      showmi: false,
+      er: false,
+      showmiHei: false,
+      NameCorrect: true
+    }
+  },
+  methods: {
+    async click () {
+      const response = await fetch('http://178.62.171.236/menus/3920', { mode: 'no-cors' })
+      if (response) {
+        console.log(response)
+      } else {
+        console.log(111)
+      }
+      console.log(2)
+      this.UserCorrect = false
+      const dar = document.getElementById('log')
+      const pas = document.getElementById('pas')
+      if (dar.value) {
+        this.users.forEach(element => {
+          if (element.name === dar.value && element.pas === pas.value) {
+            this.UserCorrect = true
+            pr = element.money
+          }
+        })
+      }
+      if (this.UserCorrect === false) {
+        this.showmi = true
+      } else if (dar.value && pas.value) {
+        this.$emit('clickBTN', dar.value, pr)
+        dar.value = ''
+        pas.value = ''
+        this.showmi = false
+      } else {
+        this.showmi = true
+      }
+    },
+    submit () {
+      this.click()
+    }
+  }
+}
+</script>
+
+<style scoped>
+input{
+    border: 5px solid #981500;
+    width: 20vw;
+    height:6vh;
+    font-size: 5vh;
+    margin-top: 2%;
+    border-radius: 10px;
+    color: #483916;
+    outline: none;
+}
+input:hover{
+  border-color: #ff4c4c;
+}
+.text{
+  font-size: 12vh;
+  color: #e28a61;
+}
+.but{
+    font-size: 8vh;
+    margin-top: 10%;
+    background-color: #ffa377;
+    outline: none;
+    border: none;
+    border-radius: 10px;
+}
+.but:hover{
+      background-color: #ffc6ab;
+}
+.reg{
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.mistake{
+  font-size: 30px;
+  padding:10px;
+  margin-left: 10px;
+  color:#b11000;
+}
+</style>
